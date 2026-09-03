72824183397acdd67d6009ccb2a3cf60c1303a88
IT26102091 <it26102091@my.sliit.lk>
2026-09-03 14:34:15 +0530

    Initial project setup

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("========================================\n");
+    printf("      ADVANCED NAVAL BATTLE SIMULATOR\n");
+    printf("========================================\n");
+
+    printf("Welcome to the Naval Battle Simulator!\n");
+
+    return 0;
+}
