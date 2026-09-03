00ffb02ceff01afc274796007e9f4a1ddf7173e4
IT26102091 <it26102091@my.sliit.lk>
2026-09-04 02:27:39 +0530

    Add projectile motion calculations

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 #define MAX_ESCORTS 100
+#define GRAVITY 9.81
+#define PI 3.14159265358979323846
 
 typedef struct
 {
@@ -19,6 +22,29 @@ typedef struct
     double y;
 } Battleship;
 
+
+/* Calculate the horizontal range of a projectile */
+double calculateRange(double velocity, double angle)
+{
+    double angleRadians;
+
+    angleRadians = angle * PI / 180.0;
+
+    return (velocity * velocity * sin(2 * angleRadians)) / GRAVITY;
+}
+
+
+/* Calculate the total time of flight of a projectile */
+double calculateTimeOfFlight(double velocity, double angle)
+{
+    double angleRadians;
+
+    angleRadians = angle * PI / 180.0;
+
+    return (2 * velocity * sin(angleRadians)) / GRAVITY;
+}
+
+
 int main()
 {
     int D;
@@ -32,25 +58,34 @@ int main()
 
     srand(time(NULL));
 
+
+    /* Program title */
     printf("========================================\n");
     printf("      ADVANCED NAVAL BATTLE SIMULATOR\n");
     printf("========================================\n\n");
+
 
     /* Get battlefield size */
     printf("Enter battlefield size D: ");
     scanf("%d", &D);
 
+
     /* Get number of escort ships */
     printf("Enter number of escort ships: ");
     scanf("%d", &N);
 
+
+    /* Check maximum number of escort ships */
     if (N > MAX_ESCORTS)
     {
-        printf("Maximum number of escort ships is %d.\n", MAX_ESCORTS);
+        printf("Maximum number of escort ships is %d.\n",
+               MAX_ESCORTS);
+
         return 1;
     }
 
-    /* Select battleship */
+
+    /* Select Battleship */
     printf("\nSelect Battleship:\n");
     printf("1. USS Iowa\n");
     printf("2. MS King George V\n");
@@ -60,49 +95,72 @@ int main()
     printf("Enter choice: ");
     scanf("%d", &battleshipChoice);
 
+
+    /* Check Battleship choice */
     if (battleshipChoice < 1 || battleshipChoice > 4)
     {
         printf("Invalid battleship choice.\n");
+
         return 1;
     }
 
-    /* Store battleship type */
-    if (battleshipChoice == 1)
-        B.type = 'U';
-    else if (battleshipChoice == 2)
-        B.type = 'M';
-    else if (battleshipChoice == 3)
-        B.type = 'R';
-    else
-        B.type = 'S';
 
-    /* Get battleship starting position */
+    /* Store Battleship type */
+    if (battleshipChoice == 1)
+    {
+        B.type = 'U';
+    }
+    else if (battleshipChoice == 2)
+    {
+        B.type = 'M';
+    }
+    else if (battleshipChoice == 3)
+    {
+        B.type = 'R';
+    }
+    else
+    {
+        B.type = 'S';
+    }
+
+
+    /* Get Battleship starting position */
     printf("\nEnter Battleship X position: ");
     scanf("%lf", &B.x);
 
     printf("Enter Battleship Y position: ");
     scanf("%lf", &B.y);
 
-    /* Generate escort ships */
+
+    /* Generate Escort ships */
     for (int i = 0; i < N; i++)
     {
         escorts[i].id = i + 1;
 
-        escorts[i].x = (double)rand() / RAND_MAX * D;
-        escorts[i].y = (double)rand() / RAND_MAX * D;
+        escorts[i].x =
+            (double)rand() / RAND_MAX * D;
+
+        escorts[i].y =
+            (double)rand() / RAND_MAX * D;
 
         int typeIndex = rand() % 5;
+
         escorts[i].type = escortTypes[typeIndex];
     }
 
-    /* Display initial battlefield information */
+
+    /* Display initial conditions */
     printf("\n========== INITIAL CONDITIONS ==========\n");
 
     printf("\nBattlefield: %d x %d\n", D, D);
 
     printf("Battleship Type: %c\n", B.type);
-    printf("Battleship Position: (%.2f, %.2f)\n", B.x, B.y);
 
+    printf("Battleship Position: (%.2f, %.2f)\n",
+           B.x, B.y);
+
+
+    /* Display Escort ships */
     printf("\nEscort Ships:\n");
 
     for (int i = 0; i < N; i++)
@@ -113,6 +171,26 @@ int main()
                escorts[i].x,
                escorts[i].y);
     }
+
+
+    /* Projectile calculation test */
+    printf("\n========== PROJECTILE TEST ==========\n");
+
+    double testVelocity = 100.0;
+    double testAngle = 45.0;
+
+    printf("Velocity: %.2f m/s\n",
+           testVelocity);
+
+    printf("Angle: %.2f degrees\n",
+           testAngle);
+
+    printf("Range: %.2f m\n",
+           calculateRange(testVelocity, testAngle));
+
+    printf("Time of flight: %.2f seconds\n",
+           calculateTimeOfFlight(testVelocity, testAngle));
+
 
     return 0;
 }
